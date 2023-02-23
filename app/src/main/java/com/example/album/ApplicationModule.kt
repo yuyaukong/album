f92d1609fe6f98d971d26da48c8a100757db0d9c
yuyaukong <yuyaukong@gmail.com>
2023-02-23 22:45:38 +0800

    Add application context
    Add context for the PreferenceUtils use

---
+++ app/src/main/java/com/example/album/ApplicationModule.kt
@@ -0,0 +1,18 @@
+package com.example.album
+
+import android.app.Application
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ApplicationModule{
+
+    @Singleton
+    @Provides
+    fun provideContext(application: Application): Context = application.applicationContext
+}
